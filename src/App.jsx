4b2f6d7f43ddf490eb3ceb814eab6eb05b1eeaf9
Jeremy <e1155908@u.nus.edu>
2025-03-04 01:29:38 +0800

    setup: background colors

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <p class="text-5xl font-bold underline">
+      <p class="text-5xl font-bold">
         Habit Tracker
       </p>
     </>
